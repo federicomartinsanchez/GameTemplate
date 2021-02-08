1839d008c380ccfcffbca81d43d0efec229eba11
Federico Martín Sánchez <fedekite1@gmail.com>
2021-02-08 01:12:05 +0100

    Added code for Audio Manager. Added code for generic singleton. Added prefabs of MAinMenu, SettingsMenu and AudioManager.

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : GenericSingleton<AudioManager>
+{
+    public Sound[] Sounds;
+
+    public override void Awake()
+    {
+        base.Awake();
+        foreach (Sound s in Sounds)
+        {
+            s.Source = gameObject.AddComponent<AudioSource>();
+            s.Source.clip = s.Clip;
+            s.Source.volume = s.Volume;
+            s.Source.pitch = s.Pitch;
+            s.Source.loop = s.Loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound sound = Array.Find(Sounds, s => s.Name == name);
+
+        if (sound == null)
+        {
+            Debug.LogWarning($"Sound {name} not found!");
+            return;
+        }
+
+        sound.Source.Play();
+    }
+}
